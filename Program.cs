6c259f976d89c9cad2261aec56ea84796f3ab4c1
Valentinooo46 <valeartem@gmail.com>
2025-02-12 21:24:57 +0200

    ДЗ.ITSTEP/Робота з Базами Даних

--- Program.cs
+++ Program.cs
@@ -1,15 +1,69 @@
-﻿using System.Text;
+﻿using System;
 using System.Data.SqlClient;
 
-Console.InputEncoding = Encoding.UTF8;
-Console.OutputEncoding = Encoding.UTF8;
-EmployeeManager employeeManager = new EmployeeManager("*", "*", "*", "*");
-Employee._id = employeeManager.LastKey();
-employeeManager.LoadSqlConnectionToJson(@"Sql_Example\appsetting.json");
-employeeManager.Create(Employee.Generate(fax:"+380993714545",PIB:"Valentinooo 54"));
-employeeManager.Create(Employee.Generate(fax: "+380993714545", PIB: "Valentinooo 55"));
-employeeManager.Create(Employee.Generate(fax: "+380993714545", PIB: "Valentinooo 56"));
-Console.WriteLine(employeeManager.Read(1).PIB);
-Console.WriteLine(employeeManager.Read(2).PIB);
-employeeManager.Delete(1);
-employeeManager.Update(new Employee(id:2,fax: "+380993714545", PIB: "Valentinooo 57"));
+class Program
+{
+    static void Main()
+    {
+
+        string connectionString = "workstation id=*;packet size=4096;user id=*;pwd=*;data source=*;persist security info=False;initial catalog=*;TrustServerCertificate=True";
+
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string createTableQuery = @"
+                IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Animals')
+                CREATE TABLE Animals (
+                    Id INT PRIMARY KEY IDENTITY,
+                    Name NVARCHAR(50),
+                    Species NVARCHAR(50),
+                    Age INT,
+                    ArrivalDate DATE
+                )";
+            using (SqlCommand command = new SqlCommand(createTableQuery, connection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+
+
+        //using (SqlConnection connection = new SqlConnection(connectionString))
+        //{
+        //    connection.Open();
+
+        //    for (int i = 1; i <= 1000; i++)
+        //    {
+        //        string insertQuery = @"
+        //            INSERT INTO Animals (Name, Species, Age, ArrivalDate)
+        //            VALUES (@Name, @Species, @Age, @ArrivalDate)";
+
+        //        using (SqlCommand command = new SqlCommand(insertQuery, connection))
+        //        {
+        //            command.Parameters.AddWithValue("@Name", "Animal" + i);
+        //            command.Parameters.AddWithValue("@Species", "Species" + (i % 10));
+        //            command.Parameters.AddWithValue("@Age", i % 15);
+        //            command.Parameters.AddWithValue("@ArrivalDate", DateTime.Now.AddDays(-i));
+
+        //            command.ExecuteNonQuery();
+        //        }
+        //    }
+        //}
+        using (SqlConnection connection = new SqlConnection(connectionString))
+        {
+            connection.Open();
+
+            string selectQuery = "SELECT * FROM Animals";
+            using (SqlCommand command = new SqlCommand(selectQuery, connection))
+            {
+                using (SqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"Id: {reader["Id"]}, Name: {reader["Name"]}, Species: {reader["Species"]}, Age: {reader["Age"]}, ArrivalDate: {reader["ArrivalDate"]}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Query1.sql
@@ -0,0 +1,15 @@
+﻿IF NOT EXISTS (SELECT * FROM sysobjects WHERE name='Animals')
+                CREATE TABLE Animals (
+                    Id INT PRIMARY KEY IDENTITY,
+                    Name NVARCHAR(50),
+                    Species NVARCHAR(50),
+                    Age INT,
+                    ArrivalDate DATE
+                );
+
+
+INSERT INTO Animals (Name, Species, Age, ArrivalDate)
+                   VALUES ('Teddy', 'Dog', 3, '2025-11-25');
+
+
+SELECT * FROM Animals;
