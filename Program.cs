5f8905ab97a0a9880764ae97b523ea4e5576a141
Valentinooo46 <valeartem@gmail.com>
2025-02-26 23:56:59 +0200

    Створення реальної БД.ITSTEP/SQL/C#

--- Program.cs
+++ Program.cs
@@ -1,11 +1,63 @@
-﻿using System.Text;
+﻿using OrderSystem;
+using System;
 using System.Data.SqlClient;
-using Sql_Example;
+using System.Text;
 
-Console.InputEncoding = Encoding.UTF8;
-Console.OutputEncoding = Encoding.UTF8;
-USERS_CH_Manager cH_Manager = new USERS_CH_Manager("*");
-//cH_Manager.INSERT_RANDOM_GENERATED_USERS(10000);
-//Console.WriteLine(cH_Manager.WATCHDOG_TIMER_SELECT_ALL_USERS());
-cH_Manager.SEARCH_USERS();
-//Console.WriteLine(cH_Manager.GetDatabaseSize());
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+        Console.InputEncoding = Encoding.UTF8;
+        string ConString = "*";
+        //Product product = new Product();
+        //product.Description = null;
+        //for (int i = 0; i < 10; i++)
+        //{ 
+        //    product.Name = $"Product{i + 1}";
+        //    product.Price = 1000;
+        //    product.CategoryId = 1;
+
+        //    product.CreatedDate = DateTime.Now;
+        //    product.SaveProduct(ConString);
+        //}
+        //User user = new User();
+        //for (int i = 0; i < 10; i++)
+        //{
+
+        //    user.FirstName = $"John{i+1}";
+        //    user.LastName = $"Dee{i}";
+        //    user.Email = $"JohnDee{i+1}@gmail.com";
+        //    user.PhoneNumber = "1234567890";
+        //    user.Password = "1234567890";
+        //    user.AddUser(ConString);
+        //}
+
+        //Order order = new Order();
+
+        //for (int i = 0; i < 10; i++)
+        //{
+        //    order.UserId = i + 1;
+        //    order.OrderStatusId = 1;
+        //    order.CreatedDate = DateTime.Now;
+
+
+        //    order.AddOrderItem(1,i+1);
+        //    order.AddOrderItem(2, i + 2);
+        //    order.AddOrderItem(3, i + 3);
+        //    order.SaveOrder(ConString);
+
+        //}
+        DatabaseQueries databaseQueries = new DatabaseQueries(ConString);
+        var result = databaseQueries.GetAverageProductPrice();
+        Console.WriteLine($"Average Product Price: {result}");
+        var result2 = databaseQueries.GetUsersWithTotalSpentMoreThan(500);
+        Console.WriteLine("Users with total spent more than 500:");
+        foreach (var item in result2)
+        {
+            Console.WriteLine($"User Id: {item.Id},User Email:{item.Email}");
+        }
+        
+
+    }
+}
